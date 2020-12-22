56f65d754ef02d8150c3c6cc8d6ee430d8602733
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2020-12-22 11:31:12 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,35 +1,38 @@
-// document.getElementById("section--86216").style.transition = "opacity 0.6s";
+document.getElementById("section--86216").style.transition = "opacity 0.6s";
 
-// var visibleY = function(el){
-//   var rect = el.getBoundingClientRect(), top = rect.top, height = rect.height, 
-//     el = el.parentNode;
-//   do {
-//     rect = el.getBoundingClientRect();
-//     if (top <= rect.bottom === false) return false;
-//     // Check if the element is out of view due to a container scrolling
-//     if ((top + height) <= rect.top) return false
-//     el = el.parentNode;
-//   } while (el != document.body);
-//   // Check its within the document viewport
-//   return top <= document.documentElement.clientHeight;
-// };
+var visibleY = function(el){
+  var rect = el.getBoundingClientRect(), top = rect.top, height = rect.height, 
+    el = el.parentNode;
+  do {
+    rect = el.getBoundingClientRect();
+    console.log("test"+top+" "+rect.bottom);
+    if (top <= rect.bottom === false) return false;
+    // Check if the element is out of view due to a container scrolling
+    console.log("test2"+top+""+height+" "+rect.bottom+" "+rect.top);
+    if ((top + height) <= rect.top) return false
+    el = el.parentNode;
+  } while (el != document.body);
+  // Check its within the document viewport
+  console.log("hasggsadjhdjas"+top+" "+document.documentElement.clientHeight);
+  return top <= document.documentElement.clientHeight;
+};
 
-// // Stuff only for the demo
-// function attachEvent(element, event, callbackFunction) {
-//     if (element.addEventListener) {
-//         element.addEventListener(event, callbackFunction, false);
-//     } else if (element.attachEvent) {
-//         element.attachEvent('on' + event, callbackFunction);
-//     }
-// };
+// Stuff only for the demo
+function attachEvent(element, event, callbackFunction) {
+    if (element.addEventListener) {
+        element.addEventListener(event, callbackFunction, false);
+    } else if (element.attachEvent) {
+        element.attachEvent('on' + event, callbackFunction);
+    }
+};
 
-// var update = function(){
+var update = function(){
 
-//     var fadeTarget = document.getElementById("section--86216");
-// console.log(visibleY(document.getElementById('headline-57978')))
-//     visibleY(document.getElementById('headline-57978')) 
-//         ? fadeTarget.style.opacity = '1' : fadeTarget.style.opacity = '0';
-// };
-// attachEvent(document, "scroll", update);
-// attachEvent(window, "resize", update);
-// update();
+    var fadeTarget = document.getElementById("section--86216");
+console.log(visibleY(document.getElementById('headline-57978')))
+    visibleY(document.getElementById('headline-57978')) 
+        ? fadeTarget.style.opacity = '1' : fadeTarget.style.opacity = '0';
+};
+attachEvent(document, "scroll", update);
+attachEvent(window, "resize", update);
+update();
