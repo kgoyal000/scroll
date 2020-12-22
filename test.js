97555852df7efbe1212974300c55176944639c1a
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2020-12-22 21:44:44 +0530

    Update test.js

--- test.js
+++ test.js
@@ -64,7 +64,7 @@ function myFunction() {
 window.addEventListener('scroll', function() {    
               var doc = document.documentElement;
               scroll =  (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-
+console.log(scroll)
              if (scroll >= 1300) {
                  //clearHeader, not clearheader - caps H
                  document.querySelector(".floating-bar[data-hide-on='desktop']").classList.add("showup");
@@ -77,6 +77,7 @@ window.addEventListener('scroll', function() {
 window.addEventListener('scroll', function() {    
               var doc = document.documentElement;
               var scroll =  (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+  console.log(scroll)
              if (scroll >= 2200) {
                  //clearHeader, not clearheader - caps H
                  document.querySelector(".floating-bar[data-hide-on='mobile']").classList.add("showup");
@@ -91,7 +92,7 @@ if (screen.width > 860){
 window.addEventListener('scroll', function() {    
               var doc = document.documentElement;
               scroll =  (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-
+console.log(scroll)
              if (scroll >= 1300) {
                  //clearHeader, not clearheader - caps H
                  document.querySelector(".floating-bar[data-hide-on='desktop']").classList.add("showup");
@@ -104,6 +105,7 @@ window.addEventListener('scroll', function() {
 window.addEventListener('scroll', function() {    
               var doc = document.documentElement;
               var scroll =  (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+              console.log(scroll)
              if (scroll >= 2400) {
                  //clearHeader, not clearheader - caps H
                  document.querySelector(".floating-bar[data-hide-on='mobile']").classList.add("showup");
